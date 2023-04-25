508a0f85f7c47e5578ccfe3a63eb25f5ca3278f2
Anushka Tyagi <anushka.tyagi161998@gmail.com>
2023-04-25 18:47:11 +0530

    pyCrawler

---
+++ app.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import requests
+response = requests.get(
+    "https://epaper.jagran.com/epaper/25-Apr-2023-64-kanpur-edition-kanpur-page-11.html")
+soup = BeautifulSoup(response.text, "lxml")
+
+
+# link = soup.select(".highlightParent")
+print(response.text)
