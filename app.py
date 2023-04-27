19bc61ff8cddcb76d79b3c565a4317b438118aa4
Anushka Tyagi <anushka.tyagi161998@gmail.com>
2023-04-27 14:12:20 +0530

    crawler working for kanpur

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from PIL import Image
 
 date = "27-Apr-2023"
 cityCode = "64"
-city = "delhi-city"
+city = "kanpur"
 pageResponse = requests.get(
     "https://epaper.jagran.com/epaper/edition-today-" + cityCode + "-" + city + ".html")
 soup = BeautifulSoup(pageResponse.text, "lxml")
@@ -18,34 +18,32 @@ soup = BeautifulSoup(pageResponse.text, "lxml")
 noOfPages = soup.select('.info')
 pages = int(str(noOfPages[0]).split('\n')[4].split(' ')[-1][0:2])
 
-for page in pages:
-response = requests.get(
-    "https://epaper.jagran.com/epaper/" + date + "-" + cityCode + "-" + city + "-edition-" + city + "-page-" + page + ".html")
-soup = BeautifulSoup(response.text, "lxml")
+for page in range(1, pages+1):
+    response = requests.get(
+        "https://epaper.jagran.com/epaper/" + date + "-" + cityCode + "-" + city + "-edition-" + city + "-page-" + str(page) + ".html")
+    soup = BeautifulSoup(response.text, "lxml")
 
 # extract paper link
-id = "#image" + page
-imageTag = soup.select(id)
-link = str(imageTag[0].get('data-src'))
-responseImg = requests.get(link)
+    id = "#image" + str(page)
+    imageTag = soup.select(id)
+    link = str(imageTag[0].get('data-src'))
+    responseImg = requests.get(link)
 
-
-with open("imagepy.jpg", "wb") as f:
-    f.write(responseImg.content)
-
-
-method = cv2.TM_SQDIFF_NORMED
-small_image = cv2.imread('icon3.jpeg')
-large_image = cv2.imread('imagepy.jpg')
-w, h = large_image.shape[:-1]
-
-res = cv2.matchTemplate(small_image, large_image, cv2.TM_CCOEFF_NORMED)
-threshold = .8
-loc = np.where(res >= threshold)
-a, b = loc
-if len(a) != 0 and len(b) != 0:
-    with open("images/imagepy.jpg", "wb") as f:
+    with open("imagepy.jpg", "wb") as f:
         f.write(responseImg.content)
+
+    method = cv2.TM_SQDIFF_NORMED
+    small_image = cv2.imread('icon4.jpeg')
+    large_image = cv2.imread('imagepy.jpg')
+    w, h = large_image.shape[:-1]
+
+    res = cv2.matchTemplate(small_image, large_image, cv2.TM_CCOEFF_NORMED)
+    threshold = .8
+    loc = np.where(res >= threshold)
+    a, b = loc
+    if len(a) != 0 and len(b) != 0:
+        with open("images/imagepy.jpg", "wb") as f:
+            f.write(responseImg.content)
 
 # for pt in zip(*loc[::-1]):  # Switch columns and rows
 #     cv2.rectangle(large_image, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
