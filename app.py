5e109f518bd214adf5f5458e4553ae2d315b34db
Anushka Tyagi <anushka.tyagi161998@gmail.com>
2023-05-01 15:08:34 +0530

    crawler complete

--- app.py
+++ app.py
@@ -3,40 +3,44 @@ import requests
 import time
 import pytesseract
 import re
-import os
 
 import cv2
 import numpy as np
 from PIL import Image
 from datetime import datetime
-
+import os
 
 cities = ["kanpur", "Lucknow", "delhi-city", "Patna-Nagar", "varanasi-city",
           "Prayagraj-City", "Gorakhpur-City", "Agra", "Meerut", "bhagalpur", "muzaffarpur-nagar"]
-date = "15-Apr-2023"
+date = "29-Apr-2023"
 cityCodes = ["64", "11", "4", "84", "45",
              "79", "56", "193", "29", "205", "203"]
 
 dictionary = {'Apr': '04'}
+# cityDictionary = {"delhi-city" : "Delhi" ,"Patna-Nagar" : "Patna" , varanasi-city }
 
 
-for i in range(0, len(cities)):
+for i in range(1, len(cities)):
     counter = 0
     city = cities[i]
     cityCode = cityCodes[i]
     pageResponse = requests.get(
-        "https://epaper.jagran.com/epaper/edition-today-" + cityCode + "-" + city + ".html")
+        "https://epaper.jagran.com/epaper/" + date + "-" + cityCode + "-" + city + "-edition-" + city + "-page-" + "1" + ".html")
     soup = BeautifulSoup(pageResponse.text, "lxml")
 
-    noOfPages = soup.select('.info')
-    pages = int(str(noOfPages[0]).split('\n')[4].split(' ')[-1][0:2])
+    # noOfPages = soup.select('.info')
+    # pages = int(str(noOfPages[0]).split('\n')[4].split(' ')[-1][0:2])
 
-    for page in range(2, 14):
+    noOfPages = soup.select("#totalpageno")[0].get("value")
+
+    for page in range(1, int(noOfPages)):
+        print("https://epaper.jagran.com/epaper/" + date + "-" + cityCode +
+              "-" + city + "-edition-" + city + "-page-" + str(page) + ".html")
         response = requests.get(
             "https://epaper.jagran.com/epaper/" + date + "-" + cityCode + "-" + city + "-edition-" + city + "-page-" + str(page) + ".html")
         soup = BeautifulSoup(response.text, "lxml")
 
-        # extract paper link
+    # extract paper link
         id = "#image" + str(page)
         imageTag = soup.select(id)
         link = str(imageTag[0].get('data-src'))
@@ -46,55 +50,37 @@ for i in range(0, len(cities)):
             f.write(responseImg.content)
 
         # match image
-        if city != "bhagalpur" and city != "muzaffarpur-nagar":
-            method = cv2.TM_SQDIFF_NORMED
-            imPath = "cities/" + city + ".jpeg"
-            small_image = cv2.imread(imPath)
-            large_image = cv2.imread('imagepy.jpg')
-            w, h = large_image.shape[:-1]
+        imagePaths = ["cities/" + city + ".jpeg", "icon.jpg"]
 
-            res = cv2.matchTemplate(
-                small_image, large_image, cv2.TM_CCOEFF_NORMED)
-            threshold = .8
-            loc = np.where(res >= threshold)
-            a, b = loc
-            print(a, b)
+        method = cv2.TM_SQDIFF_NORMED
+        large_imageS = cv2.imread('imagepy.jpg',  cv2.IMREAD_UNCHANGED)
+        for size in [100, 200, 300, 400]:
+            # print(size)
+            up_width = large_imageS.shape[0] + size
+            up_height = large_imageS.shape[1] + size
 
-        # text extraction
-        filename = 'imagepy.jpg'
-        img1 = np.array(Image.open(filename))
-        text = pytesseract.image_to_string(img1, lang='eng').lower().strip()
+            up_points = (up_width, up_height)
+            large_image = cv2.resize(
+                large_imageS, up_points, interpolation=cv2.INTER_LINEAR)
 
-        print((text).strip().split(' '))
-        array = re.findall(r'[0-9]{4}-[0-9]{3}-[0-9]{4}|[+][0-9]{2}-[0-9]{3}-[0-9]{7}|[+][9,1]{2}[\s][0-9]{4}-[0-9]{6}|[0-9]{10}|\bclassifieds\b',
-                           text)
+            # text extraction
+            filename = 'imagepy.jpg'
+            img1 = np.array(large_image)
 
-        if len(a) != 0 or len(b) != 0 or len(array) != 0:
-            counter += 1
-            if (counter <= 9):
-                number = "_0"
-            else:
+            text = pytesseract.image_to_string(img1, lang="eng").lower()
+            # print(text)
+
+            array = re.findall(r'[0-9]{4}-[0-9]{3}-[0-9]{4}|[+][0-9]{2}-[0-9]{3}-[0-9]{7}|[+][9,1]{2}[\s][0-9]{4}-[0-9]{6}|[0-9]{10}',
+                               text)
+
+            if len(array) != 0:
+                counter += 1
                 number = "_"
-            dateObj = datetime.strptime(date.replace(
-                date[3:6], dictionary[date[3:6]]), '%d-%m-%Y')
-            formateDate = dateObj.strftime("%Y-%m-%d")
-            print(formateDate)
-            with open("images/DJ_" + city + "_" + formateDate + number + str(counter) + "_hin.jpeg", "wb") as f:
-                f.write(responseImg.content)
-            # with open(os.path.dirname(os.path.abspath(__file__)) + "/images/DJ_" + city + "_" + formateDate + number + str(counter) + "_hin.jpeg", "wb") as f:
-            #     f.write(responseImg.content)
+                dateObj = datetime.strptime(date.replace(
+                    date[3:6], dictionary[date[3:6]]), '%d-%m-%Y')
+                formateDate = dateObj.strftime("%Y-%m-%d")
+                formatCity = city.lower().replace("-city", "").replace("-nagar", "").capitalize()
 
-
-# for pt in zip(*loc[::-1]):  # Switch columns and rows
-#     cv2.rectangle(large_image, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-# cv2.imwrite('result.png', large_image)
-
-
-# write to text file
-# f = open("imageLink.txt", "w")
-# link = str(imageTag[0].get('data-src'))
-# f.write(link)
-
-
-# f = open("lang.txt", "w", encoding="utf-8")
-# f.write(str(text))
+                with open("images/DJ_" + formatCity + "_" + formateDate + number + str(counter) + "_hin.jpeg", "wb") as f:
+                    f.write(responseImg.content)
+                break
